fb57385017ca02238c7c163465558c07f84c8409
Vinícius Giroti <121032547+Vinicius2901@users.noreply.github.com>
2023-05-11 15:05:32 -0300

    Create ordemv.c

---
+++ ordemv.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, n, tam, menor = 2*pow (10, 8), pos, *gru;
+    printf ("Digite o tamanho do vetor: ");
+    scanf (" %i", &tam);
+    gru = malloc (tam * sizeof(int));
+    printf("Digite os elementos do vetor: ");
+    if (gru != NULL)
+    {
+        for (i = 0; i < tam; i++)
+        {
+            scanf(" %i", &gru[i]);
+        }
+        for (i = 0; i < tam; i++)
+        {
+            if (gru[i] < menor)
+            {
+                menor = gru[i];
+                pos = i;
+            }
+        }
+        for (i = 0; i < tam; i++)
+        {
+            if (gru[i] == menor)
+            {
+                gru[i] = gru[0];
+                gru[0] = menor;
+            }
+        }
+        printf ("[");
+        for (i = 0; i < tam; i++)
+        {
+            if (i < tam - 1)
+            {
+                printf ("%i, ", gru[i]);
+            }
+            else
+            {
+                printf ("%i]", gru[i]);
+            }
+        }
+    }
+    else
+    {
+        return 1;
+    }
+    return 0;
+}
