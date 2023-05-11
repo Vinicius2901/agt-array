39ed7fc551af2fdb1605722b1d8f1d46dc242084
Vinícius Giroti <121032547+Vinicius2901@users.noreply.github.com>
2023-05-11 14:53:29 -0300

    Update somaparnv.c com ponteiros

--- somaparnv.c
+++ somaparnv.c
@@ -1,34 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    int i, n, tam;
-    printf ("Digite um número inteiro: ");
+    int i, n, tam, *gru;
+    printf ("Digite um numero inteiro: ");
     scanf (" %i", &n);
     printf ("Digite o tamanho do vetor: ");
     scanf (" %i", &tam);
-    tam = tam + 1;
-    int gru[tam];
+    gru = malloc(tam * sizeof(int));
     printf ("Digite os elementos do vetor: ");
-    for (i = 0; i < tam - 1; i++)
+    if (gru != NULL)
     {
-        scanf (" %i", &gru[i]);
-    }
-    printf ("v = [");
-    for (i = 0; i < tam - 1; i += 2)
-    {
-        gru [i] = gru[i] + n;
-    }
-    for (i = 0; i < tam - 1; i++)
-    {
-        if (i < tam - 2)
+        for (i = 0; i < tam; i++)
         {
-            printf ("%i,", gru[i]);
+            scanf (" %i", &gru[i]);
         }
-        else
+        printf ("v = [");
+        for (i = 0; i < tam; i += 2)
         {
-            printf ("%i", gru[i]);
+            gru [i] = gru[i] + n;
         }
+        for (i = 0; i < tam; i++)
+        {
+            if (i < tam - 1)
+            {
+                printf ("%i,", gru[i]);
+            }
+            else
+            {
+                printf ("%i", gru[i]);
+            }
+        }
+    }
+    else
+    {
+        return 1;
     }
     printf ("]\n");
     return 0;
