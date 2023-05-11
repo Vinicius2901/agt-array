9a50509829765663535263c33035dccc3a7ccbaf
Vinícius Giroti <121032547+Vinicius2901@users.noreply.github.com>
2023-05-11 14:54:00 -0300

    Update menor_posicao.c com ponteiros

--- menor_posicao.c
+++ menor_posicao.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 int main()
 {
-    int i, n, tam, menor = 2*pow (10, 8), pos;
+    int i, n, tam, menor = 2*pow (10, 8), pos, *gru;
     printf ("Digite o tamanho do vetor: ");
     scanf (" %i", &tam);
-    int gru[tam];
+    gru = malloc (tam * sizeof(int));
     printf("Digite os elementos do vetor: ");
-    for (i = 0; i < tam - 1; i++)
+    if (gru != NULL)
     {
-        scanf(" %i", &gru[i]);
-    }
-    for (i = 0; i < tam - 1; i++)
-    {
-        if (gru[i] < menor)
+        for (i = 0; i < tam; i++)
         {
-            menor = gru[i];
-            pos = i;
+            scanf(" %i", &gru[i]);
         }
+        for (i = 0; i < tam; i++)
+        {
+            if (gru[i] < menor)
+            {
+                menor = gru[i];
+                pos = i;
+            }
+        }
+    }
+    else
+    {
+        return 1;
     }
     printf ("Menor = %i\n", menor);
     printf ("Posicao: %i\n", pos);
